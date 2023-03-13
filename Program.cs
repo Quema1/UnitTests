ad601d23364c95dafe02ad8230dbac8df227b8eb
Олексій Морочковський <oleksii.morochkovskyi@nure.ua>
2023-03-13 15:40:45 +0200

    three

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UnitTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringCalculator calc = new StringCalculator();
+            Console.WriteLine(calc.Add("1\n2,3?1")); 
+        }
+    }
+}
---
+++ StringCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    class StringCalculator
+    {
+        public int Add(string numbers)
+        {
+            string[] separators = new string[] { ",", ".", "/", "/n", "\n", "?" };
+            int result = 0;
+            if (numbers.Length == 0)
+                return result;
+            if (numbers.Length == 1)
+            {
+                result += Convert.ToInt32(new string(numbers.FirstOrDefault(), 1));
+                return result;
+            }
+
+            string[] array = numbers.Split(separators, StringSplitOptions.None);
+            foreach (var el in array)
+            {
+                result += Convert.ToInt32(el);
+            }
+            return result;
+        }
+    }
+}
